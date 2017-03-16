448087b4a27b82c87c77e20aa0d03d74e0d65ebf
NUS57160156 <se57160156@gmail.com>
2017-03-16 11:10:15 +0700

    Initial commit

---
+++ app/src/main/java/com/rnus/mysqlapplication/MainActivity.java
@@ -0,0 +1,73 @@
+package com.rnus.mysqlapplication;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public ListView listView;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.TodoListview);
+
+    }
+
+    protected void onResume(){
+        super.onResume();
+        TodoListDAO todoListDAO = new TodoListDAO(getApplication());
+        todoListDAO.open();
+        ArrayList<TodoList> myList = todoListDAO.getAlltodoList();
+
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                //android.R.layout.simple_list_item_1, myList);
+
+        final MyListView adapter = new MyListView(this, myList);
+        listView.setAdapter(adapter);
+        todoListDAO.close();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent editIntent = new Intent(getApplicationContext(), EditDataActivity.class);
+
+                editIntent.putExtra("editTodoList",(Serializable) adapter.getItem(position));
+                startActivity(editIntent);
+
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id== R.id.action_add_new_menu){
+            Intent addNewIntent = new Intent (this,AddDataActivity.class);
+            startActivity(addNewIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
